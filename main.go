31e813648d241a85dc3a9a81f2257e3b7180b4d8
onibakuman <david.attid@gmail.com>
2018-01-12 01:22:19 -0700

    Updated git ignore and vagrant file to account for golangs install

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+// fun main()
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
